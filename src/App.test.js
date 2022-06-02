9823bc3b7f1d173cf42c44efd772443c4339dd59
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-06-02 19:57:21 +0700

    Add test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { render, cleanup, screen } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let getByTestId;
+
+//Seolah2 aplikasi counter muncul/tampil
+beforeEach(() => {
+  // eslint-disable-next-line testing-library/no-render-in-setup
+  const view = render(<App />);
+  getByTestId = view.getByTestId;
+});
+
+//Definis test
+test('header render with correct text', () => {
+  //bentuk awal
+  // const view = render(<App />);
+  // const pickByTest = view.getAllByTestId
+
+  //Untuk menunjuk komponen yang ditunjuk berdasarkan testid
+  const headerEl = screen.getByTestId('header');
+
+  expect(headerEl.textContent).toBe('Counter App');
 });
